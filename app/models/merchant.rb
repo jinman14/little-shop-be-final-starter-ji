1023bbe74c13c918dbe6df0678ccfb48d4865f76
Jacob Inman <jrinman6295@gmail.com>
2025-03-10 16:27:49 -0700

    merchant coupon count functional

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -19,6 +19,10 @@ class Merchant < ApplicationRecord
     items.count
   end
 
+  def coupon_count
+    coupons.count
+  end
+
   def distinct_customers
     # self.customers.distinct # This is possible due to the additional association on line 5
     
