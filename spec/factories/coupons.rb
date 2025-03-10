0fdb0621c1cf2ad3ebfc656d4bd1aa3309c38fa0
Jacob Inman <jrinman6295@gmail.com>
2025-03-10 09:27:20 -0700

    added Faker for name on coupon factory

--- spec/factories/coupons.rb
+++ spec/factories/coupons.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :coupon do
-    name { "Coo-pin Time" }
+    name { Faker::Name.name }
     code { "#{SecureRandom.hex(4)}" }
     discount { 10.0 }
     association :merchant
